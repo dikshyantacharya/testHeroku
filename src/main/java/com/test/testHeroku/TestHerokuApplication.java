afaf0a916f046b46d1d794032f5b32a9547f731e
NOVOMIND\dacharya <Shrey@dhung@n@123>
2022-06-04 08:28:00 +0200

    testCommit

---
+++ src/main/java/com/test/testHeroku/TestHerokuApplication.java
@@ -0,0 +1,14 @@
+package com.test.testHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHerokuApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestHerokuApplication.class, args);
+  }
+
+}
+
